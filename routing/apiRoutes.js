266713528a1cfd9e34a9f6cfc373ce2b81298869
nicksessa <50333367+nicksessa@users.noreply.github.com>
2019-08-20 15:11:22 -0400

    Added logic to find the best match
    Added the logic to find the best match.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -19,19 +19,81 @@ module.exports = function (app) {
 
     // API POST Requests
     // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
+
     // ---------------------------------------------------------------------------
 
     app.post("/api/friends", function (req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body parsing middleware
+        // This function receives an array, which is the results of the survey.
 
-        friendList.push(req.body);
-        //  need this?  res.json(false);
+        console.log("Name passed: " + req.userName)
+        console.log("link to photo: " + req.userPhoto)
+
+
+
+
+var compatibility = [];
+
+
+for (var i = 0; i < friendList.length; i++ ) {
+  var obj = friendList[i];
+  var name = obj.name
+  //console.log("Name: " + name)
+
+  var overallScore = 0
+
+  var answers = obj.scores
+  for (var x = 0; x < answers.length; x++) {
+    var friendAnswer = answers[x]
+    //console.log("answer [" + x + "] --> " + answers[x])
+
+    var userAnswer = req.scores[x]
+    //console.log("  User Answer [" + x + "] --> " + req.scores[x])
+    
+    theDiff = Math.abs(friendAnswer - userAnswer)
+    //console.log("The Difference: " + theDiff )
+
+
+    console.log(name + " ans[" + x + "] " + friendAnswer + " u_ans: " + userAnswer + " diff: " + theDiff )
+
+    overallScore += theDiff
+  }
+  console.log("overall Score: " + overallScore)
+  console.log("---------------------------------")
+
+  compatibility.push([name, overallScore]);
+
+}
+
+// sort the array
+
+compatibility.sort(function(a, b) {
+  return a[1] - b[1]
+})
+
+for (var i = 0; i < compatibility.length; i++) {
+  for (var j = 0; j < compatibility[i].length; j++) {
+    console.log('[' + i + ',' + j + '] = ' + compatibility[i][j])
+  }
+}
+
+var bestMatchName = compatibility[0][0]
+console.log("Best Match: " + bestMatchName)
+
+var bestMatch = friendList.filter(function(x) { 
+  return x.name == bestMatchName;
+});
+
+console.log("Photo URL: " + bestMatch[0].photo)
+
+
+
+
+
+
+
+
+        //friendList.push(req.body);   There is no need to push anything to the friendList array.
+        //  need this?  res.json(false);  // we could return true if a match is found or false if not
 
     });
 }
