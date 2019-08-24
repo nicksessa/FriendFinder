dd876f2c27896cf7eafd71a6db878d6e9fd520f5
NickSessa <nicholas.sessa@gmail.com>
2019-08-23 21:40:01 -0400

    added to the friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,68 +1,116 @@
 var friendList = [
-    {
-        "name":"Grace Kelly",
-        "photo":"http://d3v3hv0xzfxuou.cloudfront.net/wp-content/uploads/2018/07/19130031/grace-kelly-princesa-com-joias-1-953x715.jpg",
-        "scores":[
-            2,
-            1,
-            4,
-            1,
-            5,
-            1,
-            2,
-            1,
-            2,
-            1
-          ]
-      },
-      {
-        "name":"Jane Seymoure",
-        "photo":"https://imgix.ranker.com/user_node_img/50041/1000810616/original/young-jane-seymour-in-black-and-gold-dress-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces",
-        "scores":[
-            3,
-            1,
-            2,
-            4,
-            3,
-            1,
-            2,
-            5,
-            4,
-            5
-          ]
-      },
-      {
-        "name":"Sean Connery",
-        "photo":"https://cbsnews1.cbsistatic.com/hub/i/r/2006/03/13/4b94f364-a642-11e2-a3f0-029118418759/resize/620x465/5319b4094a3f42d0d6d68bc1c70cbf59/image1393123.jpg",
-        "scores":[
-            1,
-            1,
-            2,
-            5,
-            5,
-            3,
-            2,
-            1,
-            4,
-            1
-          ]
-      },
-      {
-        "name":"Dean Martin",
-        "photo":"https://img.discogs.com/nb5x2y-LPLLSUsr9EWwLKCTLnUc=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-10533-1106230820.jpg.jpg",
-        "scores":[
-            5,
-            5,
-            4,
-            4,
-            5,
-            5,
-            2,
-            2,
-            3,
-            1
-          ]
-      }   
+  {
+    "name": "Grace Kelly",
+    "photo": "http://d3v3hv0xzfxuou.cloudfront.net/wp-content/uploads/2018/07/19130031/grace-kelly-princesa-com-joias-1-953x715.jpg",
+    "scores": [
+      2,
+      1,
+      4,
+      1,
+      5,
+      1,
+      2,
+      1,
+      2,
+      1
+    ]
+  },
+  {
+    "name": "Lauren Bacall",
+    "photo": "https://i.pinimg.com/originals/c8/07/ce/c807ce2e4cfcc9019eb3fc74f58e6d19.png",
+    "scores": [
+      2,
+      1,
+      4,
+      1,
+      3,
+      1,
+      4,
+      1,
+      2,
+      4
+    ]
+  },
+  {
+    "name": "Humphrey Bogart",
+    "photo": "https://static.tvtropes.org/pmwiki/pub/images/humphrey_bogart_01.jpg",
+    "scores": [
+      4,
+      2,
+      4,
+      1,
+      5,
+      1,
+      3,
+      2,
+      2,
+      1
+    ]
+  },
+  {
+    "name": "Ingrid Bergman",
+    "photo": "https://i.pinimg.com/originals/d3/30/64/d330641e991e9040c6f0a4aabe58d33e.jpg",
+    "scores": [
+      3,
+      1,
+      4,
+      2,
+      5,
+      1,
+      4,
+      1,
+      2,
+      5
+    ]
+  },
+  {
+    "name": "Jane Seymoure",
+    "photo": "https://imgix.ranker.com/user_node_img/50041/1000810616/original/young-jane-seymour-in-black-and-gold-dress-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces",
+    "scores": [
+      3,
+      1,
+      2,
+      4,
+      3,
+      1,
+      2,
+      5,
+      4,
+      5
+    ]
+  },
+  {
+    "name": "Sean Connery",
+    "photo": "https://cbsnews1.cbsistatic.com/hub/i/r/2006/03/13/4b94f364-a642-11e2-a3f0-029118418759/resize/620x465/5319b4094a3f42d0d6d68bc1c70cbf59/image1393123.jpg",
+    "scores": [
+      1,
+      1,
+      2,
+      5,
+      5,
+      3,
+      2,
+      1,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Dean Martin",
+    "photo": "https://img.discogs.com/nb5x2y-LPLLSUsr9EWwLKCTLnUc=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-10533-1106230820.jpg.jpg",
+    "scores": [
+      5,
+      5,
+      4,
+      4,
+      5,
+      5,
+      2,
+      2,
+      3,
+      1
+    ]
+  }
 ]
 
 module.exports = friendList;
